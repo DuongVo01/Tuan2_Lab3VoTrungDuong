4f63ee90dbdad88722156ec0e95375a2fffc3dee
DuongVo01 <votrungduong1@gmail.com>
2024-06-03 15:29:36 +0700

    Initial commit

---
+++ src/main/java/Tuan2/VoTrungDuong/VoTrungDuongApplication.java
@@ -0,0 +1,13 @@
+package Tuan2.VoTrungDuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoTrungDuongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoTrungDuongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tuan2/VoTrungDuong/VoTrungDuongApplicationTests.java
@@ -0,0 +1,13 @@
+package Tuan2.VoTrungDuong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoTrungDuongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
